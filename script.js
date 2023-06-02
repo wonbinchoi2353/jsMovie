daaed777eab1a27c294bc33589129c8f39e34202
wonbin choi <fnffnqls@naver.com>
2023-06-02 12:53:58 +0900

    added id button function, search function

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function getMovies() {
     return movies;
 }
 
-// async await로 객체 쓸 수있게 만드는 함수
+// async await로 객체 쓸 수 있게 만드는 함수
 async function setMoives() {
     let movies = await getMovies();
     moviesRender(movies);
@@ -70,47 +70,62 @@ function imgsRender(images) {
     let poster_sizes = images.images.poster_sizes[4];
     // console.log(base_url + poster_sizes);
     return base_url + poster_sizes;
-
-    // 리턴값 다른 함수에서 어떻게 쓰는지 모르겠음
 }
-// let a = imgsRender(images)
-// console.log(a)
+
+
+// 검색 input
+let searchInput = document.getElementById("search-input")
+// 검색 버튼
+let searchBtn = document.getElementById("search-btn")
+
+// 이 부분 혼자서 한 번 보고 다시 만들어보기(튜터님이 구현)
+searchBtn.addEventListener("click", async () => {
+    document.getElementById("grid").innerHTML = "";
+    const serachInput = searchInput.value;
+
+    let movies = await getMovies();
+    movies = movies.results;
+    movies.results = movies.filter(movie => movie.original_title.includes(serachInput))
+    moviesRender(movies);
+});
 
 
 // 영화 데이터
-function moviesRender(movies) {
+async function moviesRender(movies) {
+    // getImgs에서 객체 가져오기
+    let images = await getImgs()
+    let image = imgsRender(images)
+    // movies 배열 순회하면서 원하는 데이터 변수에 저장
     movies.results.forEach((movie) => {
-        // if () {}
+        // if () {} include, filter
         let card_id = movie.id;
         let poster_path = movie.poster_path;
         let title = movie.title;
         let overview = movie.overview;
         let vote_average = movie.vote_average;
+        let imgPath = image + poster_path;
 
-        // 다른 함수의 리턴값 쓰기 모르겠음
-        // let imgPath = images;
-        // console.log(imgPath)
-        // imgPath = imgPath + poster_path;
-
-        let imgPath = "http://image.tmdb.org/t/p/w500" + poster_path;
-
-        // 카드 복제
+        // 카드 생성
         cloneCard(card_id, imgPath, title, overview, vote_average);
-
-
-        // 클릭하면 검색
-        function idAlert(card_id) {
-            let card = document.getElementById("card")
-            card.addEventListener("click", function () {
-                alert(card_id);
-            });
-        }
     });
-}
 
-// id값 경고창
-idAlert(card_id);
+    // 이 부분 혼자서 한 번 보고 다시 만들어보기(튜터님이 구현)
+    // 카드를 클릭했을때 alert을 뛰우는 기능
+    // 1. 내가 카드를 선택해야한다.
+    // 2. 선택한 카드에 클릭이벤트를 걸어야한다
+    // 3. 클릭이 되었을때 alert을 뛰워야한다.
+    let cards = document.querySelectorAll(".movie-card");
+    console.log(cards)
+    cards.forEach(card => {
+        card.addEventListener("click", function (event) {
+            console.log(event.currentTarget)
+            console.dir(event.currentTarget)
+            alert(event.currentTarget.id)
+        });
+    });
+};
 
+// 카드 생성
 function cloneCard(card_id, imgPath, title, overview, vote_average) {
     // section 선택
     let section = document.getElementById("grid");
@@ -152,78 +167,12 @@ function cloneCard(card_id, imgPath, title, overview, vote_average) {
 
     // 카드의 id값 설정
     setCard.id = String(card_id);
+
+
 }
 
-
-
-
-
-
-// include
-
-
-
-
-
-// // DOM api
-// // `document.createElement(tagName)` : 새로운 HTML 요소를 생성합니다.
-// // `document.getElementById(id)` : id 속성을 기준으로 요소를 선택합니다.
-// // `document.getElementsByTagName(name)` : 태그 이름을 기준으로 요소를 선택합니다.
-// // `document.getElementsByClassName(name)` : 클래스 이름을 기준으로 요소를 선택합니다.
-// // `document.querySelector(selector)` : CSS 선택자를 이용하여 요소를 선택합니다.
-// // `document.querySelectorAll(selector)` : CSS 선택자를 이용하여 모든 요소를 선택합니다.
-// //  문서 객체 조작
-// // `element.innerHTML` : 해당 요소 내부의 HTML 코드를 변경합니다.
-// // `element.textContent` : 해당 요소 내부의 텍스트를 변경합니다.
-// // `element.setAttribute(attr, value)` : 해당 요소의 속성 값을 변경합니다.
-// // `element.getAttribute(attr)` : 해당 요소의 속성 값을 가져옵니다.
-// // `element.style.property` : 해당 요소의 스타일 값을 변경합니다.
-// // `element.appendChild(child)` : 해당 요소의 하위 요소로 child를 추가합니다.
-// // `element.removeChild(child)` : 해당 요소의 하위 요소 중 child를 삭제합니다.
-// // `element.classList.add(class)` : 해당 요소의 클래스에 새로운 클래스를 추가합니다.
-// // `element.classList.remove(class)` : 해당 요소의 클래스 중에서 특정 클래스를 제거합니다.
-// // `element.classList.toggle(class)` : 해당 요소의 클래스 중에서 특정 클래스를 추가 또는 제거합니다.
-// //  이벤트 처리
-// // `element.addEventListener(type, listener)` : 해당 요소에서 이벤트가 발생했을 때 호출할 함수를 등록합니다.
-// // `element.removeEventListener(type, listener)` : 해당 요소에서 등록된 함수를 제거합니다.
-// // `event.preventDefault()` : 이벤트가 발생했을 때 기본 동작을 취소합니다.
-// // `event.stopPropagation()` : 이벤트의 버블링을 방지하기 위해 이벤트 전파를 중지합니다.
-// //  기타
-// // `window.location.href` : 현재 페이지의 URL을 가져옵니다.
-// // `window.alert(message)` : 경고 메시지를 출력합니다.
-// // `window.confirm(message)` : 확인 메시지를 출력하고 사용자의 답변에 따라 Boolean 값을 반환합니다.
-
-
-
-
-
-
-// // // 카드 복제
-// function cloneCard(card_id, img_path, title, overview, vote_average) {
-//     // 원본 card 선택
-//     let card = document.getElementById("card");
-//     // 해당 node의 children 까지 복제
-//     let copy = card.cloneNode(true);
-//     // 카드 section 선택
-//     let section = document.getElementById("grid");
-//     // 해당 요소의 하위 요소로 copy 추가
-//     section.appendChild(copy);
-
-//     // 이미지, 제목, 설명, 평점 id를 선택
-//     let chg_img = document.getElementById("img");
-//     let chg_title = document.getElementById("title");
-//     let chg_overview = document.getElementById("overview");
-//     let chg_vote_average = document.getElementById("vote_average");
-
-//     // 해당 요소 내부의 이미지, 텍스트 변경
-//     chg_img.setAttribute("src", img_path);
-//     chg_title.textContent = title;
-//     chg_overview.textContent = overview;
-//     chg_vote_average.textContent = "Rating: " + vote_average;
-
-//     // 새로운 카드의 id 부여
-//     copy.id = String(card_id);
-// };
-
-
-// // let element = document.createElement(div)
+// 카드 업데이트
+// function cardsRender(movies) {
+//     let b = document.querySelector('.card_list')
+//     b.innerHTML = '';
+// }
